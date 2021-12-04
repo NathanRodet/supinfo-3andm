b3eb0043c34608dad3a9e58ece9ff187feecfb74
NathanRodet <nathan.rodet@hardis-group.com>
2021-12-04 23:41:48 +0100

    commit tablayout

--- app/src/main/java/com/supinfo/supfitness/activities/TrackActivity.kt
+++ app/src/main/java/com/supinfo/supfitness/activities/TrackActivity.kt
@@ -1,12 +1,31 @@
 package com.supinfo.supfitness.activities
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.supinfo.supfitness.R
 
 class TrackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_track)
+
+        // TabLayout Navigation
+        val buttonWeight: Button = findViewById<Button>(R.id.buttonWeight)
+        val buttonTrack: Button = findViewById<Button>(R.id.buttonTrack)
+        val buttonChart: Button = findViewById<Button>(R.id.buttonChart)
+        buttonTrack.setBackgroundColor(Color.parseColor("#FF018786"))
+
+        buttonWeight.setOnClickListener(){
+            startActivity(Intent(this, WeightActivity::class.java))
+        }
+        buttonTrack.setOnClickListener(){
+            startActivity(Intent(this, TrackActivity::class.java))
+        }
+        buttonChart.setOnClickListener(){
+            startActivity(Intent(this, ChartActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/supinfo/supfitness/activities/WeightActivity.kt
+++ app/src/main/java/com/supinfo/supfitness/activities/WeightActivity.kt
@@ -1,12 +1,34 @@
 package com.supinfo.supfitness.activities
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.supinfo.supfitness.R
+import kotlinx.android.synthetic.main.tab_layout.*
 
 class WeightActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TabLayout Navigation
+        val buttonWeight: Button = findViewById<Button>(R.id.buttonWeight)
+        val buttonTrack: Button = findViewById<Button>(R.id.buttonTrack)
+        val buttonChart: Button = findViewById<Button>(R.id.buttonChart)
+        buttonWeight.setBackgroundColor(Color.parseColor("#FF018786"))
+
+        buttonWeight.setOnClickListener(){
+            startActivity(Intent(this, WeightActivity::class.java))
+        }
+        buttonTrack.setOnClickListener(){
+            startActivity(Intent(this, TrackActivity::class.java))
+        }
+        buttonChart.setOnClickListener(){
+            startActivity(Intent(this, ChartActivity::class.java))
+        }
+
     }
 }
