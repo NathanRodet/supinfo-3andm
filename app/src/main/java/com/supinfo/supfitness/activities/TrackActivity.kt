0b3d3e94e7a6ef4c108e13cc2463c6e2859c0b13
NathanRodet <nathan.rodet@hardis-group.com>
2021-12-11 00:07:05 +0100

    implementing google maps

--- app/src/main/java/com/supinfo/supfitness/activities/TrackActivity.kt
+++ app/src/main/java/com/supinfo/supfitness/activities/TrackActivity.kt
@@ -5,17 +5,31 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.supinfo.supfitness.R
 
-class TrackActivity : AppCompatActivity() {
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.supinfo.supfitness.R
+import com.supinfo.supfitness.databinding.ActivityTrackBinding
+
+class TrackActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityTrackBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_track)
 
+        binding = ActivityTrackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         // TabLayout Navigation
         val buttonWeight: Button = findViewById(R.id.buttonWeight)
@@ -23,14 +37,33 @@ class TrackActivity : AppCompatActivity() {
         val buttonChart: Button = findViewById(R.id.buttonChart)
         buttonTrack.setBackgroundColor(Color.parseColor("#33018786"))
 
-        buttonWeight.setOnClickListener{
+        buttonWeight.setOnClickListener {
             startActivity(Intent(this, WeightActivity::class.java))
         }
-        buttonTrack.setOnClickListener{
+        buttonTrack.setOnClickListener {
             startActivity(Intent(this, TrackActivity::class.java))
         }
-        buttonChart.setOnClickListener{
+        buttonChart.setOnClickListener {
             startActivity(Intent(this, ChartActivity::class.java))
         }
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
 }
